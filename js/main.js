03145f2f5dab4ba239bfb72cd4023ea26e2095b1
Prosefal <6167402isroil@gmail.com>
2022-12-07 11:29:50 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,60 @@
+// ------------------------------------1-masala
+
+// function addition(a, b) {
+//   return a + b;
+// }
+
+// -------------------------------------2-masala
+
+// function convert(minutes) {
+//   var secund = 60
+//   return secund * minutes
+// }
+
+
+// --------------------------------------3-masala
+
+// function addition(num) {
+//   return ++num
+// }
+
+
+// -------------------------------------4-masala
+
+// function triArea(base, height) {
+//   return base * height / 2;
+// }
+
+
+// --------------------------------------5-masala
+
+//  function calcAge(age) {
+//   return age*365;
+// }
+
+
+// -------------------------------------  6-masala
+
+// function remainder(x, y) {
+//   return x % y;
+// }
+
+
+// ---------------------------------------7-masala
+
+// function footballPoints(wins, draws, losses) {
+//   return wins* 3 + draws* 1;
+// }
+
+// ----------------------------------------9-masala
+
+// function lessThan100(a, b) {
+//   return a + b < 100;
+// }
+
+
+// ----------------------------------------10-masala
+
+// function animals(chickens, cows, pigs) {
+//   return chickens * 2 + cows * 4 + pigs * 4;
+// }
